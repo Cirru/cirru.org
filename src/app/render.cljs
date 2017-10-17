20bbe482af7d56c256ff132d005f054033d36f12
jiyinyiyong <jiyinyiyong@gmail.com>
2017-10-17 22:31:17 +0800

    upgrade deps; update CDN

--- src/app/render.cljs
+++ src/app/render.cljs
@@ -10,25 +10,24 @@
 (def base-info
   {:title "Cirru: an editor for AST",
    :icon "http://repo-cdn.b0.upaiyun.com/logo/cirru.png",
-   :ssr nil,
-   :inline-html "<link href=\"https://fonts.googleapis.com/css?family=Hind+Vadodara|Josefin+Sans:300,400\" rel=\"stylesheet\" />"})
+   :ssr nil})
 
 (defn prod-page []
   (let [html-content (make-string (comp-container schema/store))
-        manifest (.parse js/JSON (slurp "dist/assets-manifest.json"))
-        cljs-manifest (.parse js/JSON (slurp "dist/manifest.json"))
-        cdn (if previews? "" "http://repo-cdn.b0.upaiyun.com/cirru.org/")
+        webpack-info (.parse js/JSON (slurp "dist/webpack-manifest.json"))
+        cljs-info (.parse js/JSON (slurp "dist/cljs-manifest.json"))
+        cdn (if previews? "" "http://cdn.tiye.me/cirru.org/")
         cdn-prefix (fn [x] (str cdn x))]
     (make-page
      html-content
      (merge
       base-info
-      {:styles [(cdn-prefix (aget manifest "main.css"))],
+      {:styles ["http://cdn.tiye.me/favored-fonts/main.css"
+                (cdn-prefix (aget webpack-info "main.css"))],
        :scripts (map
                  cdn-prefix
-                 [(aget manifest "main.js")
-                  (-> cljs-manifest (aget 0) (aget "js-name"))
-                  (-> cljs-manifest (aget 1) (aget "js-name"))]),
+                 [(-> cljs-info (aget 0) (aget "js-name"))
+                  (-> cljs-info (aget 1) (aget "js-name"))]),
        :ssr "respo-ssr"}))))
 
 (defn dev-page []
@@ -36,7 +35,8 @@
    ""
    (merge
     base-info
-    {:styles [], :scripts ["/main.js" "/browser/lib.js" "/browser/main.js"]})))
+    {:styles ["http://localhost:8100/main.css"],
+     :scripts ["/main.js" "/browser/lib.js" "/browser/main.js"]})))
 
 (defn main! []
   (if (= js/process.env.env "dev")
